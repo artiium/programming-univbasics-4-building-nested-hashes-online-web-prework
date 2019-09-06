9d5eb4879f7f3476500fc27683ee557f6086ffb4
Artiium Arbyummi <artiium21@gmail.com>
2019-09-06 17:17:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
   monopoly = {:railroads => {:pieces => 4,
   :rent_in_dollars => {
     :one_pieced_owned => "25",
-    :two_owner =>
+    :two_pieces_owned => "50",
     :three_owner =>
     :four_owner =>
   }
