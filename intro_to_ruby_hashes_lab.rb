862d10f029f4db979d4f673a695aa215d61e3faa
Artiium Arbyummi <artiium21@gmail.com>
2019-09-06 17:10:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,8 +10,8 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  monopoly = {:railroads => {:pieces => 4},
-  :rent_in_dollars
+  monopoly = {:railroads => {:pieces => 4,
+  :rent_in_dollars =>
     
     
     
