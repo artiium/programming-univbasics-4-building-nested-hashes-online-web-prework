748a2f9a5323eb27a19d9159fc9fd386ebeeb794
Artiium Arbyummi <artiium21@gmail.com>
2019-09-06 16:31:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,7 @@
 def base_hash
-  arbyummi_family = {
+  college_football_teams = {
+    :sec => {"Alabama" => {:mascot => "Roll Tide", :colors => "Red"}
+    
     
   }
   # Remember implicit return! We're returning this new Hash _without_ the
